baac2583224d959094cc0048bd7f58befe276633
Benjamin Lupton <b@lupton.cc>
2015-09-21 14:05:10 +0800

    latest esnextguardian code

--- esnextguardian.js
+++ esnextguardian.js
@@ -1,6 +1,3 @@
 // 18 September 2015
 // https://github.com/bevry/base
-module.exports = require('esnextguardian')(
-	require('path').join(__dirname, 'esnext', 'lib', 'index.js'),
-	require('path').join(__dirname, 'es5', 'lib', 'index.js')
-)
+module.exports = require('esnextguardian')('./esnext/lib/index.js', './es5/lib/index.js', require)
