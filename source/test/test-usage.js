29d67c1fc8de110c67a154ebadde909c696c1a6a
Benjamin Lupton <b@lupton.cc>
2016-06-04 06:13:27 +0800

    more robust error catching, still failing however

--- source/test/test-usage.js
+++ source/test/test-usage.js
@@ -1,58 +1,88 @@
 // Import
 const joe = require('joe')
-const {expectErrorViaFunction, throwErrorViaCallback, returnViaCallback, completeViaCallback, expectViaCallback, expectErrorViaCallback} = require('assert-helpers')
+const {equal, expectErrorViaFunction, throwErrorViaCallback, returnViaCallback, completeViaCallback, expectViaCallback, expectErrorViaCallback} = require('assert-helpers')
 const {wait} = require('./test-util')
 const {Task, TaskGroup} = require('../')
 
+
 // Prepare
 const delay = 100
+
+function bump (checks) {
+	if ( checks.i == null )  checks.i = 0
+	if ( checks.n == null )  checks.n = 0
+	++checks.n
+	return () => ++checks.i
+}
+
+function bumped (checks, next, testName) {
+	if ( checks.i == null )  checks.i = 0
+	if ( checks.n == null )  checks.n = 0
+	wait(delay * 2, () => {
+		equal(checks.i, checks.n, testName || 'all expected checks ran')
+		if ( next )  next()
+	})
+}
+
 
 // Task
 joe.suite('task', function (suite, test) {
 	// failure: done with no run
 	test('Task.create(...).done(...) should time out when run was not called', function (complete) {
+		const checks = {}
 		Task.create(returnViaCallback(5))
 			.done(throwErrorViaCallback('unexpected error'))
-		wait(delay, complete)
+		bumped(checks, complete)
 	})
 
 	// failure: done with no task method
 	test('Task.create().run().done(...) should fail as there was no task method defined', function (complete) {
+		const checks = {}
 		Task.create()
 			.run()
-			.done(expectErrorViaCallback('no method', 'error was as expected', complete))
+			.done(expectErrorViaCallback('no method', 'error was as expected', bump(checks)))
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: run then done
 	test('Task.create(...).run().done(...) should fire the completion callback with the expected result', function (complete) {
+		const checks = {}
 		Task.create(returnViaCallback(5))
 			.run()
 			.done(expectViaCallback(null, 5))
-			.done(complete)
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: done then run
 	test('Task.create(...).done(...).run() should fire the completion callback with the expected result', function (complete) {
+		const checks = {}
 		Task.create(returnViaCallback(5))
 			.run()
 			.done(expectViaCallback(null, 5))
-			.done(complete)
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// failure: done then run then run
 	test('Task.create(...).done(...).run().run() should fail as a task is not allowed to run twice', function (complete) {
+		const checks = {}
 		Task.create(returnViaCallback(5))
-			.on('error', expectErrorViaCallback('run status', 'error was emitted and caught by the listener expected', complete))
+			.on('error', expectErrorViaCallback('run status', 'error was emitted and caught by the listener expected', bump(checks)))
 			.run().run()
+		bumped(checks, complete)
 	})
 
 	// failure: run then run then done
 	test('Task.create(...).run().run().done(...) should fail as a task is not allowed to run twice', function (complete) {
+		const checks = {}
 		expectErrorViaFunction('run status', function () {
 			Task.create(returnViaCallback(5))
 				.run().run()
 				.on('error', throwErrorViaCallback('unexpected error'))
-		}, 'error was uncaught by the error listener as expected, because the error listener was bound after the error was emitted', complete)
+		}, 'error was uncaught by the error listener as expected, because the error listener was bound after the error was emitted', bump(checks))
+		bumped(checks, complete)
 	})
 })
 
@@ -60,18 +90,22 @@ joe.suite('task', function (suite, test) {
 joe.suite('taskgroup', function (suite, test) {
 	// failure: done with no run
 	test('TaskGroup.create().addTask(...).done(...) should time out when run was not called', function (complete) {
+		const checks = {}
 		TaskGroup.create()
 			.addTask(returnViaCallback(5))
 			.done(throwErrorViaCallback('unexpected error'))
-		wait(delay, complete)
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: done with no tasks then run
 	test('TaskGroup.create().run().done(...) should complete with no result', function (complete) {
+		const checks = {}
 		TaskGroup.create()
 			.run()
 			.done(expectViaCallback(null, []))
-			.done(complete)
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	/*
@@ -97,6 +131,7 @@ joe.suite('taskgroup', function (suite, test) {
 
 	// success: multiple runs
 	test('Taskgroup should be able to complete multiple times with destroyOnceDone: false', function (complete) {
+		const checks = {}
 		const tasks = TaskGroup.create({destroyOnceDone: false})
 			.addTask(returnViaCallback(5))
 			.run()
@@ -105,42 +140,54 @@ joe.suite('taskgroup', function (suite, test) {
 			tasks
 				.addTask(returnViaCallback(10))
 				.done(expectViaCallback(null, [[null, 5], [null, 10]]))
-				.done(complete)
+				.done(bump(checks))
 		})
+		bumped(checks, complete)
 	})
 
 	// success: run then done then add
 	test('TaskGroup.create().run().done(...).addTask(...) should complete with the tasks result', function (complete) {
+		const checks = {}
 		TaskGroup.create()
 			.run()
 			.done(expectViaCallback(null, [[null, 5]]))
-			.done(complete)
+			.done(bump(checks))
 			.addTask(returnViaCallback(5))
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: done then task then run then done
 	test('TaskGroup.create().done().addTask(...).run().addTask(...).done(...) should complete correctly', function (complete) {
+		const checks = {}
 		TaskGroup.create()
 			.done(expectViaCallback(null, [[null, 5], [null, 10]]))
+			.done(bump(checks))
 			.addTask('task 1 that will return 5', returnViaCallback(5))
 			.run()
 			.done(expectViaCallback(null, [[null, 5], [null, 10]]))
+			.done(bump(checks))
 			.addTask('task 2 that will return 10', returnViaCallback(10))
-			.done(complete)
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: done then task then run then done
 	test('TaskGroup.create().run().run().done(...) should complete only once', function (complete) {
+		const checks = {}
 		TaskGroup.create()
 			.done(expectViaCallback(null, [[null, 5], [null, 10]]))
+			.done(bump(checks))
 			.addTask(returnViaCallback(5))
 			.run().run()
 			.addTask(returnViaCallback(10))
-			.done(complete)
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: pause after error
 	test('Taskgroup should pause when encountering an error', function (complete) {
+		const checks = {}
 		const err = new Error('fail after 5')
 		TaskGroup.create()
 			.addTask(returnViaCallback(5))
@@ -148,13 +195,13 @@ joe.suite('taskgroup', function (suite, test) {
 			.addTask(returnViaCallback(10))
 			.run()
 			.done(expectViaCallback(err, [[null, 5], [err]]))
-			.done(function () {
-				complete()
-			})
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// success: resume after error
 	test('Taskgroup should be able to resume after an error', function (complete) {
+		const checks = {}
 		const err = new Error('fail after 5')
 		const tasks = TaskGroup.create()
 			.addTask(returnViaCallback(5))
@@ -162,17 +209,20 @@ joe.suite('taskgroup', function (suite, test) {
 			.addTask(returnViaCallback(10))
 			.run()
 			.done(expectViaCallback(err, [[null, 5], [err]]))
+			.done(bump(checks))
 		// @TODO a new run should be required here... that clears the previously completed results
 		wait(delay, function () {
 			tasks
 				.addTask(returnViaCallback(15))
 				.done(expectViaCallback(null, [[null, 5], [err], [null, 10], [null, 15]]))
-				.done(complete)
+				.done(bump(checks))
 		})
+		bumped(checks, complete)
 	})
 
 	// success: ignore after error
 	test('Taskgroup should ignore when encountering an error with different config', function (complete) {
+		const checks = {}
 		const err = new Error('fail after 5')
 		TaskGroup.create({onError: 'ignore'})
 			.addTask(returnViaCallback(5))
@@ -182,13 +232,13 @@ joe.suite('taskgroup', function (suite, test) {
 			.done(expectViaCallback(null, [
 				[null, 5], [err], [null, 10]
 			]))
-			.done(function () {
-				complete()
-			})
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 
 	// failure: nested timeouts
 	test('Taskgroup should apply nested configuration to tasks', function (complete) {
+		const checks = {}
 		TaskGroup.create()
 			.setConfig({
 				nestedTaskConfig: {
@@ -200,7 +250,9 @@ joe.suite('taskgroup', function (suite, test) {
 			.addTask(completeViaCallback(10, delay * 2))
 			.addTask(returnViaCallback(15))
 			.run()
-			.done(expectErrorViaCallback('timed out', 'error was as expected', complete))
+			.done(expectErrorViaCallback('timed out', 'error was as expected', bump(checks)))
+			.done(bump(checks))
+		bumped(checks, complete)
 	})
 })
 
